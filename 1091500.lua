37c39badd7a9ae19d99ed27f39b89c77d2d30e7d
ZackTheGrumpy <amritsingh57573@gmail.com>
2025-07-22 15:31:12 +0800

    Initial commit for 1091500

---
+++ 1091500.lua
@@ -0,0 +1,79 @@
+-- Generated Lua Manifest by Morrenus
+-- Steam App 1091500 Manifest
+-- Name: Cyberpunk 2077
+-- Generated: 2025-07-18 23:36:40
+-- Total Depots: 25
+-- Total DLCs: 4
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1091500, 1, "cf941dce25dfe9090877d385fca0ded473ecc1e12a09e28698b5c8dd3c134598") -- Cyberpunk 2077
+
+-- MAIN APP DEPOTS
+addappid(1091501, 1, "fd4524cb30582752f15e4bf01254dbb7b890f58008101a40208cd78fb139d800") -- Cyberpunk 2077 Content
+setManifestid(1091501, "6814019010317238099", 0)
+addappid(1091502, 1, "90dd5a17a45ff0595a7ed6366d0fea078e8c362633ebbe6382c173d736063b72") -- Cyberpunk 2077 Speech PL
+setManifestid(1091502, "6184739646416384219", 0)
+addappid(1091503, 1, "b2606cbfc79b86f848e75dbf59afeeae8a1d5c08523111dcdbeace51b69ec48c") -- Cyberpunk 2077 Speech RU
+setManifestid(1091503, "2155431883450560521", 0)
+addappid(1091504, 1, "c874647c72c73e87686ecef42bd4c0fbc8e564be98bb0c20c487807c2c4d8d47") -- Cyberpunk 2077 Speech FR
+setManifestid(1091504, "154478943248480549", 0)
+addappid(1091505, 1, "8382ac2e50bd6c716f748eefe9aa9a7e4ccafe5f4f2d7a19f01e5d5d2d096864") -- Cyberpunk 2077 Speech IT
+setManifestid(1091505, "2785239419527800155", 0)
+addappid(1091506, 1, "794d47675f8d91a58528e4b7e0bb347d1c542ccdd48eeda5a796b7c1af663e08") -- Cyberpunk 2077 Speech DE
+setManifestid(1091506, "4950498239915244227", 0)
+addappid(1091507, 1, "ad400264d25291632ad0c35e48e98ae3ada3c411a7ed946bacbdfaa80fd0dd00") -- Cyberpunk 2077 Speech ES
+setManifestid(1091507, "6688146715027058561", 0)
+addappid(1091508, 1, "fa0ebf333c7ac4828a8a38fd9c646543a872965d3bf1b5e205fa176dc1e93337") -- Cyberpunk 2077 Speech JP
+setManifestid(1091508, "8161295208627735550", 0)
+addappid(1091509, 1, "987c3b4b920a2258661e62065089d398fcc2412b300355c47a2655b4b373fafa") -- Cyberpunk 2077 Speech ZH
+setManifestid(1091509, "1164045688452124372", 0)
+addappid(1460470, 1, "97d6dfacfdc6ba0678277cfbdc4f38b561807a3fb8679513411c7b9e819195b3") -- Cyberpunk 2077 Speech KO
+setManifestid(1460470, "293694417878924697", 0)
+addappid(1460471, 1, "20f9808b7948ce1d4b7b16d660e745ce5f251bd0e40c18d8a4072d7f944eb56e") -- Cyberpunk 2077 Speech BRPT
+setManifestid(1460471, "5234987954077902546", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- Cyberpunk 2077 Bonus Content (AppID: 1495710)
+addappid(1495710)
+addtoken(1495710, "13440227389371067611")
+addappid(1495710, 1, "d1eee983c6b6dc175c3ec89ae337fe4e4435d19bf689ed1607e19f0887ca1e3c") -- Cyberpunk 2077 Bonus Content - Cyberpunk 2077 Bonus Content (1495710) Depot
+setManifestid(1495710, "8009204367296826414", 0)
+-- addappid(2638650, 1, "MISSING_KEY") -- Cyberpunk 2077 Bonus Content - Depot 2638650 (no key available)
+
+-- Cyberpunk 2077 REDmod (AppID: 2060310)
+addappid(2060310)
+addappid(2060310, 1, "45cf0829a4dae87f0f1b263c92df30079111d1dd697bb87d6830f21e04fdf8a0") -- Cyberpunk 2077 REDmod - Cyberpunk 2077 Test (2060310) Depot
+setManifestid(2060310, "6234713401507336211", 0)
+
+-- Cyberpunk 2077 Phantom Liberty (AppID: 2138330)
+addappid(2138330)
+addappid(2138330, 1, "0b2b89913e185ab0ac452b5af584c482d5a8c6f34b7b32ae2dda130deaab64bd") -- Cyberpunk 2077 Phantom Liberty - Depot 2138330
+setManifestid(2138330, "2681614462109514084", 0)
+addappid(2138331, 1, "2555f9afe152d9355bbc8153ed96c38750b8bbe66724062fde11810dbf132693") -- Cyberpunk 2077 Phantom Liberty - Depot 2138331
+setManifestid(2138331, "1515225712344831998", 0)
+addappid(2138332, 1, "4c64a4a0b1cd40244001e3d86e6fc19208ee6f1367cd117683860fcfe9e08dcb") -- Cyberpunk 2077 Phantom Liberty - Depot 2138332
+setManifestid(2138332, "7811618162995203737", 0)
+addappid(2138333, 1, "7991a1134455aead3222a58b1246e836804a0ef11faef0284867c29d77d23945") -- Cyberpunk 2077 Phantom Liberty - Depot 2138333
+setManifestid(2138333, "8973181842704151843", 0)
+addappid(2138334, 1, "7cf1934dfe03948c22b65d11c4b0df6fe4bb6f05c0b76fc6c240b730cb62c73d") -- Cyberpunk 2077 Phantom Liberty - Depot 2138334
+setManifestid(2138334, "2428287503954301647", 0)
+addappid(2138335, 1, "6388e0552ce0fc948e5c93e23902afa0ba9315d78f9eb491422abdc97b390409") -- Cyberpunk 2077 Phantom Liberty - Depot 2138335
+setManifestid(2138335, "4705845783787169441", 0)
+addappid(2138336, 1, "c318bdc3350aad3f756cadb68d2dde3954e60869c003ad15d150e0f5d95bdad9") -- Cyberpunk 2077 Phantom Liberty - Depot 2138336
+setManifestid(2138336, "5791750427517125960", 0)
+addappid(2138337, 1, "f9d3075d5d20d353263175bd5a682ff396fc3e2e59836a39e018ac81200c505f") -- Cyberpunk 2077 Phantom Liberty - Depot 2138337
+setManifestid(2138337, "8189607285917014937", 0)
+addappid(2138338, 1, "6bf9c69492004a9e232ab7c90dbdafae3ae3364cccdf4104df98f3fd02e4ddb7") -- Cyberpunk 2077 Phantom Liberty - Depot 2138338
+setManifestid(2138338, "5210757943276218215", 0)
+addappid(2138339, 1, "0d34fa3f0439c9b355882e4e366c4ff89f4e17d2cc2605a05f7473c48ca93bee") -- Cyberpunk 2077 Phantom Liberty - Depot 2138339
+setManifestid(2138339, "6383694772058460265", 0)
+addappid(2224070, 1, "13af5172058a27f8523f2caf0b4378c8dc58adf507ff3b8320ee6292ef2db899") -- Cyberpunk 2077 Phantom Liberty - Depot 2224070
+setManifestid(2224070, "4895334293375766619", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2441600) -- Phantom Liberty Quadra Vigilante Pre-Order Bonus
